4217aa4960309356623551501ef4d3df8aba5514
Jeix T <g_jesusmejias@nube.unadmexico.mx>
2019-08-08 13:58:26 -0500

    redireccion de login en pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!-- Primera vista de la pagina, tanto como administrador como 
+<!-- Primera vista de la pagina, tanto como administrador como
 usuario pueden visualizarla -->
 @extends('layouts.layout')
 @section('title','welcome')
@@ -6,7 +6,7 @@ usuario pueden visualizarla -->
     <header>
         <h1>Event Bokking Platform</h1>
 
-         <a href="{{ route('rating') }}">Login</a>
+         <a href="{{ route('login') }}">Login</a>
     </header>
 @endsection
 
